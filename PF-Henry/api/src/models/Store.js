8881b62a5049ced1925bc3dc2b628c8f06add1b7
Santiago Rambeaud <santy_r_7@hotmail.com>
2022-12-05 17:06:46 -0300

    StoreModel

---
+++ PF-Henry/api/src/models/Store.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('store', {
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+    },
+    banner: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    logo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    adress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    zip: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false
+  }
+  );
+};
