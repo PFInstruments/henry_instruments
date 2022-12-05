7165672319bea2360b9975b6ca8e285fddc19230
Andyvaz1 <andyvaz192@gmail.com>
2022-12-05 17:24:38 -0300

    Importacion de componentes footer y Navbar a componente Home

---
+++ PF-Henry/client/src/Components/Footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+    return (
+        <div>
+            <h3>Footer</h3>
+        </div>
+    );
+}
