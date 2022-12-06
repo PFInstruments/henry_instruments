a27aff4a33eb4918094876f9f29c7589cab23967
Henrique <kikework32@gmail.com>
2022-12-06 17:26:45 -0400

    middlewere orders

--- PF-Henry/api/src/routes/index.js
+++ PF-Henry/api/src/routes/index.js
@@ -6,7 +6,8 @@ const productMiddleware = require('./Product');
 const categoryMiddleware = require('./Categories');
 const userMiddleware = require('./User');
 const checkoutMiddleware = require('./Checkout');
-// Ejemplo: const authRouter = require('./auth.js');
+const orderMiddelewere = require('./Oreders')
+// Ejemplo: const auth Router = require('./auth.js');
 
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -19,6 +20,7 @@ router.use('/products', productMiddleware);
 router.use('/users', userMiddleware);
 router.use('/category', categoryMiddleware);
 router.use('/checkout', checkoutMiddleware);
+router.use('/Orders', orderMiddelewere);
 
 
 module.exports = router;
