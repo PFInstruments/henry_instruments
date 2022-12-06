4337bac819329a23b818efd91e1fde71a822ee37
Facu-J-Boy <boyfacundo45@gmail.com>
2022-12-06 15:14:55 -0300

    cambios

--- PF-Henry/api/src/routes/index.js
+++ PF-Henry/api/src/routes/index.js
@@ -3,13 +3,9 @@ const { Router } = require('express');
 
 const storeMiddleware = require('./Store');
 const productMiddleware = require('./Product');
-
 const categoryMiddleware = require('./Categories');
-
 const userMiddleware = require('./User');
-
 const checkoutMiddleware = require('./Checkout');
-
 // Ejemplo: const authRouter = require('./auth.js');
 
 // Ejemplo: const authRouter = require('./auth.js');
@@ -20,9 +16,7 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 router.use('/store', storeMiddleware);
 router.use('/products', productMiddleware);
-
 router.use('/users', userMiddleware);
-
 router.use('/category', categoryMiddleware);
 router.use('/checkout', checkoutMiddleware);
 
