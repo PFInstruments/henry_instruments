740b235bb536d42f1d4844405df1779cba9f1b95
Facu-J-Boy <boyfacundo45@gmail.com>
2022-12-06 12:24:49 -0300

    facu/back/products_routes

--- PF-Henry/api/src/routes/index.js
+++ PF-Henry/api/src/routes/index.js
@@ -2,14 +2,16 @@ const { Router } = require('express');
 // Importar todos los routers;
 
 const storeMiddleware = require('./Store');
-// Ejemplo: const authRouter = require('./auth.js');
+const productMiddleware = require('./Product');
 
+// Ejemplo: const authRouter = require('./auth.js');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.use('/store', storeMiddleware);
+router.use('/products', productMiddleware);
 
 
 module.exports = router;
