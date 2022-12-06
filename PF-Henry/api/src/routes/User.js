9bedfc15d8c5846b37180a8e04e03c15ac8a2aa9
Benjamin Miño <natanael.96.01@gmail.com>
2022-12-06 19:46:52 -0300

    ruta post /users listo y actualizacion de /users y /users/:id

--- PF-Henry/api/src/routes/User.js
+++ PF-Henry/api/src/routes/User.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const controllers = require('./controllers/User');
 
 router.get('/', async (req, res, next) => {
-    const users = await controllers.getUsers();
-    res.status(200).json({ results: users });
+    try{
+        const users = await controllers.getUsers();
+        res.status(200).json({ results: users });
+    }catch(err){
+        res.status(404).json({error: err.message});
+    }
 });
 
 router.get('/:id', async (req, res, next) => {
@@ -12,6 +16,15 @@ router.get('/:id', async (req, res, next) => {
     try{
         const user = await controllers.getUser(id);
         res.status(200).json(user);
+    }catch(err){
+        res.status(404).json({error: err.message});
+    }
+});
+
+router.post('/', async(req, res, next) => {
+    const {name, phone_num, email, password, adress, isAdmin} = req.body;
+    try{
+        res.status(201).json({details: await controllers.createUser(name, phone_num, email, password, adress, isAdmin)});
     }catch(err){
         res.status(404).json({error: err.message});
     }
