897ff321ea3685217e9a081f09c298952f0183a2
Andyvaz1 <andyvaz192@gmail.com>
2022-12-06 21:24:53 -0300

    Tailwind, Bootstrap, sass instalado e integradas las 3 tecnologias

--- PF-Henry/client/src/index.jsx
+++ PF-Henry/client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./Styles/styles.css";
+import "./Styles/tailwindStyles.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
