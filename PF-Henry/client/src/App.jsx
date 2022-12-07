f93f766caa97d107dc9571f930c4e750cb268014
Maria Jose Nielsen <mariajosenielsen@gmail.com>
2022-12-07 13:21:50 -0300

    Footer base completo

--- PF-Henry/client/src/App.jsx
+++ PF-Henry/client/src/App.jsx
@@ -1,18 +1,20 @@
 import { Route, BrowserRouter } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import AdminDashboard from "./Components/AdminDashboard/AdminDashboard";
+import Footer from "./Components/Footer/footer";
 
 import "./App.css";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <div>
-                <Route exact path="/" render={() => <Home />} />
-                <Route exact path="/admin" render={() => <AdminDashboard />} />
-            </div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" render={() => <Home />} />
+        <Route exact path="/admin" render={() => <AdminDashboard />} />
+        <Route path="/" render={() => <Footer />} />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
