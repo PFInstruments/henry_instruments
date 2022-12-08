00277b86446c70c241c33f25ebfd5ac5a376dea9
Henrique <kikework32@gmail.com>
2022-12-08 09:58:36 -0400

    formulario login (solo estilos)

--- PF-Henry/client/src/App.jsx
+++ PF-Henry/client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import AdminDashboard from "./Components/AdminDashboard/AdminDashboard";
 import Footer from "./Components/Footer/footer";
+import Login from "./Components/Login/login";
 
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
       <div>
         <Route exact path="/" render={() => <Home />} />
         <Route exact path="/admin" render={() => <AdminDashboard />} />
+        <Route exact path="/login" render={()=> <Login />}/>
         <Route path="/" render={() => <Footer />} />
       </div>
     </BrowserRouter>
---
+++ PF-Henry/client/src/Components/Login/login.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Login=()=>{
+    return(
+        <div className="container mt-3">
+            <h3>Form Validation</h3>
+            <p>Try to submit the form.</p>
+            <form className="was-validated">
+                <div className="mb-3 mt-3">
+                    <label for="uname" className="form-label">Username:</label>
+                    <input type="text" className="form-control" placeholder="introduzca correo" required/>
+                    <div className="valid-feedback">Valido.</div>
+                    <div className="invalid-feedback">Correo invalido.</div>
+                </div>
+                <div className="mb-3">
+                    <label for="pwd" className="form-label">Password:</label>
+                    <input type="password" className="form-control" placeholder="introduzca contrasena"  required/>
+                    <div className="valid-feedback">Valid0.</div>
+                    <div className="invalid-feedback">contrasena invalida.</div>
+                </div>
+                <div className="form-check mb-3">
+                    <input className="form-check-input" type="checkbox" required/>
+                    <label className="form-check-label" for="myCheck">I agree on blabla.</label>
+                    <div className="valid-feedback">Valid.</div>
+                    <div className="invalid-feedback">Check this checkbox to continue.</div>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            <br />
+            <br />
+        </div>
+    )
+}
+
+export default Login;
