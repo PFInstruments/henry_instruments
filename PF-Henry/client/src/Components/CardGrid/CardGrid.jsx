ec6644ba834810e4f83638e40ef7099dc361463d
cruzguillermo <guillermobelizcruz98@gmail.com>
2022-12-13 20:12:49 -0300

    paginado

--- PF-Henry/client/src/Components/CardGrid/CardGrid.jsx
+++ PF-Henry/client/src/Components/CardGrid/CardGrid.jsx
@@ -1,48 +1,48 @@
-//import style from './Card.module.scss';
 import React from "react";
 import Card from "../Card/Card";
+import Paginado from "../Paginado/Paginado";
+import { useState } from "react";
 
 export default function CardGrid({ localProducts }) {
-    //dispatch
-    //aqui se llama a la accion
-    console.log(localProducts);
+  //dispatch
+  //aqui se llama a la accion
+  console.log(localProducts);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(6);
+  const indexOfLast = currentPage * productsPerPage;
+  const indexOfFirst = indexOfLast - productsPerPage;
+  const currentProducts = localProducts.slice(indexOfFirst, indexOfLast);
 
-    return (
-        <div>
-            <div className="tw-grid tw-grid-cols-3 tw-gap-4 tw-m-4">
-                {localProducts &&
-                    localProducts.map((el) => {
-                        return (
-                            <Card
-                                key={el.id}
-                                id={el.id}
-                                name={el.name}
-                                image={el.image}
-                                category={el.category}
-                                price={el.price}
-                                trademark={el.trademark}
-                                model={el.model}
-                            />
-                        )
-                    })}
-            </div>
-        </div>
-        // <div className="card">
-        //     {products&&products.map((el) => (
-        //         <div className="card" key={el.id}>
-        //             <div className="card-img-top">
-        //                 <img src={el.image}
-        //                     alt={el.name} />
-        //             </div>
-        //             <div className="card-body">
-        //                 <div className="card-title">
-        //                     <h3>{el.name}</h3>
-        //                     <p>{el.description}</p>
-        //                     <p>{el.price}</p>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     ))}
-        // </div>
-    );
+  function paginado(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
+
+  return (
+    <div>
+      <div className="tw-grid tw-grid-cols-3 tw-gap-4 tw-m-4">
+        {currentProducts &&
+          currentProducts.map((el) => {
+            return (
+              <Card
+                key={el.id}
+                id={el.id}
+                name={el.name}
+                image={el.image}
+                category={el.category}
+                price={el.price}
+                trademark={el.trademark}
+                model={el.model}
+              />
+            );
+          })}
+      </div>
+      <div className="tw-flex tw-justify-center">
+       <Paginado
+        productsPerPage={productsPerPage}
+        paginado={paginado}
+        localProducts={localProducts.length}
+      />
+      </div>
+    </div>
+  );
 }
