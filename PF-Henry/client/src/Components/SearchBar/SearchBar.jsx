2f830971a0e68f3278e9e33ef90174b592c546fc
Facu-J-Boy <boyfacundo45@gmail.com>
2022-12-12 22:48:33 -0300

    facu/front/Card

--- PF-Henry/client/src/Components/SearchBar/SearchBar.jsx
+++ PF-Henry/client/src/Components/SearchBar/SearchBar.jsx
@@ -6,12 +6,30 @@ const SearchBar = ({ localProducts, setLocalproducts, allProducts }) => {
 
     const [name, setName] = useState('');
 
+    // const handleInputChange = (ev) => {
+    //     setName(ev.target.value);
+    //     if (!ev.target.value) {
+    //         setLocalproducts(allProducts);
+    //     } else {
+    //         let filter = localProducts.filter((p) => p.name.toLowerCase().indexOf(name.toLowerCase()) > -1);
+    //         setLocalproducts(filter);
+    //     };
+    // };
+
     const handleInputChange = (ev) => {
         setName(ev.target.value);
         if (!ev.target.value) {
             setLocalproducts(allProducts);
         } else {
-            let filter = localProducts.filter((p) => p.name.toLowerCase().indexOf(name.toLowerCase()) > -1);
+            // eslint-disable-next-line array-callback-return
+            let filter = localProducts.filter((product) => {
+                if (product.name.toLowerCase().indexOf(name.toLowerCase()) > -1
+                    || product.trademark.toLowerCase().indexOf(name.toLowerCase()) > -1
+                    || product.model.toLowerCase().indexOf(name.toLowerCase()) > -1
+                ) {
+                    return product;
+                }
+            });
             setLocalproducts(filter);
         };
     };
@@ -23,7 +41,7 @@ const SearchBar = ({ localProducts, setLocalproducts, allProducts }) => {
                 <nav className="navbar bg-light">
                     <div className="container-fluid">
                         <form className="d-flex" role="search">
-                            <input className="form-control me-2"
+                            <input className="form-control me-2 .ms-1"
                                 type="search"
                                 value={name}
                                 placeholder="Search product"
