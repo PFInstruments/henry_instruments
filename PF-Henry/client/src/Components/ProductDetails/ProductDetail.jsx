36c31c1b530b2c9c8a9d4c55b76f9ccc750c08f4
Maria Jose Nielsen <mariajosenielsen@gmail.com>
2022-12-13 22:02:09 -0300

    product detail con comentarios

--- PF-Henry/client/src/Components/ProductDetails/ProductDetail.jsx
+++ PF-Henry/client/src/Components/ProductDetails/ProductDetail.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { clearPageProductDetail, getProductDetail } from "../../Redux/actions";
+import {
+  clearPageProductDetail,
+  getProductDetail,
+  addToCart,
+} from "../../Redux/actions";
+import Comments from "./Comments";
 import "./productDetail.css";
 
 const ProductDetail = () => {
@@ -16,6 +21,10 @@ const ProductDetail = () => {
 
   const prDetail = useSelector((state) => state.productDetail);
   console.log(prDetail);
+
+  const handleAddToCart = () => {
+    dispatch(addToCart(id));
+  };
 
   return (
     <div className="bg-secondary product-detail">
@@ -34,19 +43,25 @@ const ProductDetail = () => {
               <h1 className="card-title my-5">{prDetail.name}</h1>
 
               <span className="card-text py-3">
-                <small className="text-muted"> Descripcion:</small>
-                <span className="text-secondary"> {prDetail.description}</span>
+                <small className="text-muted"> Marca:</small>
+                <span className="text-secondary"> {prDetail.trademark}</span>
               </span>
+
+              <span className="card-text py-3">
+                <small className="text-muted"> Modelo:</small>
+                <span className="text-secondary"> {prDetail.model}</span>
+              </span>
+
               <br />
               <span className="card-text py-3">
-                <small className="text-muted"> Precio:</small>{" "}
+                <small className="text-muted"> Precio:</small>
                 <span className="text-secondary"> AR$ {prDetail.price}</span>
               </span>
               <br />
               <span className="card-text py-3">
                 <small className="text-muted"> Categoría:</small>
                 <span className="text-secondary">
-                  como ver nombre de categoria?
+                  {prDetail.categorie?.name}
                 </span>
               </span>
               <br />
@@ -59,13 +74,31 @@ const ProductDetail = () => {
                 </span>
               </span>
               <br />
+              <span className="card-text py-3">
+                <small className="text-muted"> Descripcion:</small>
+                <span className="text-secondary"> {prDetail.description}</span>
+              </span>
+              <br />
 
-              <button className="tw-text-green-700 hover:tw-text-white tw-border tw-border-green-700 hover:tw-bg-green-800 focus:tw-ring-4 focus:tw-outline-none focus:tw-ring-green-300 tw-font-medium tw-rounded-lg tw-text-sm tw-px-5 tw-py-2.5 tw-text-center tw-mr-2 tw-mb-2 dark:tw-border-green-500 dark:tw-text-green-500 dark:hover:tw-text-white dark:hover:tw-bg-green-600 dark:focus:tw-ring-green-800">
+              <button
+                className="tw-text-green-700 hover:tw-text-white tw-border tw-border-green-700 hover:tw-bg-green-800 focus:tw-ring-4 focus:tw-outline-none focus:tw-ring-green-300 tw-font-medium tw-rounded-lg tw-text-sm tw-px-5 tw-py-2.5 tw-text-center tw-mr-2 tw-mb-2 dark:tw-border-green-500 dark:tw-text-green-500 dark:hover:tw-text-white dark:hover:tw-bg-green-600 dark:focus:tw-ring-green-800"
+                onClick={handleAddToCart}
+              >
                 Añadir al carrito
               </button>
             </div>
           </div>
         </div>
+      </div>
+      <div className="container bg-lightgrey mt-3 justify-content-center">
+        <span className="card-text py-3">
+          <small className="text-muted"> Comentarios:</small>
+          <span className="comments">
+            {prDetail.coments?.map((e) => {
+              return <Comments score={e.score} comment={e.comment} />;
+            })}
+          </span>
+        </span>
       </div>
     </div>
   );
