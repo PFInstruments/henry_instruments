bc67f412cc66d779ff60d400769ce64762e247e1
Santiago Rambeaud <santy_r_7@hotmail.com>
2022-12-15 13:40:49 -0300

    cart-modal

--- PF-Henry/client/src/Components/NavBar/NavBar.jsx
+++ PF-Henry/client/src/Components/NavBar/NavBar.jsx
@@ -8,6 +8,7 @@ let scrollHeight = {
 export default function NavBar() {
     /// ESTADO GLOBAL ////
     const { allcategories } = useSelector((state) => state);
+    const { cart } = useSelector(state => state.cart);
 
     /// LISTA DE CATEGORIAS POR ORDEN ALPHABETICO///
     let categoryList = allcategories?.map((c) => {
@@ -97,6 +98,11 @@ export default function NavBar() {
                                 >
                                     <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
                                 </svg>
+                                <span 
+                                className="badge badge-danger"
+                                style={{position: "absolute", top: "0px"}} >
+                                    {cart.length}
+                                </span>
                             </button>
                             <button
                                 type="button"
