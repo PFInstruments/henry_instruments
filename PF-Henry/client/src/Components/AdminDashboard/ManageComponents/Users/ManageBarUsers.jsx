c26bfc399a8e72d10dfc20bba3c15d78767f999f
Henrique <kikework32@gmail.com>
2022-12-17 15:32:00 -0400

    limpieza de codigo para deploy

--- PF-Henry/client/src/Components/AdminDashboard/ManageComponents/Users/ManageBarUsers.jsx
+++ PF-Henry/client/src/Components/AdminDashboard/ManageComponents/Users/ManageBarUsers.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function ManageBarUsers({}) {
+export default function ManageBarUsers(/*{}*/) {
     ////ESTADOS LOCALES//////
     // const [localOrder, setLocalOrder] = useState();
 
