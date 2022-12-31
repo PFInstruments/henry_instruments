4eeb843b81e4b93073b2bfe44f621ad42e9e010f
hackermat <villacresa@gmail.com>
2022-12-30 22:40:19 -0500

    form control rev5

--- PF-Henry/client/src/hooks/useForm.js
+++ PF-Henry/client/src/hooks/useForm.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 
 export const useForm = (initialForm, validateForm) => {
   const [form, setForm] = useState(initialForm);
@@ -18,14 +19,24 @@ export const useForm = (initialForm, validateForm) => {
     handleChance(e);
     setErrors(validateForm(form));
   };
-  
+
+  const sendEmail = (e) => {
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Your message was sent, thank you',
+      showConfirmButton: false,
+      timer: 2000
+    })
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     emailjs.sendForm('service_x6w3gw3', 'template_ypwesae', e.target, 'bqIvzfBDGrlv2Ww24' )
     .then(response => console.log(response))
     .catch(error => console.log(error))
     if(Object.keys(errors).length === 0) {
-    alert("Your message was sent, thank you");
+    sendEmail();
     //deja ern blanco el formulario
     e.target.reset();
     setForm(initialForm);
