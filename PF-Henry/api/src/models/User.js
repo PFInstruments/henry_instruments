83e0b9e1e3df1f0ea54cbb1bfd284f026294502f
Benjamin Miño <natanael.96.01@gmail.com>
2023-01-05 17:09:11 -0300

    id updated

--- PF-Henry/api/src/models/User.js
+++ PF-Henry/api/src/models/User.js
@@ -7,7 +7,7 @@ module.exports = (sequelize) => {
         id: {
             type: DataTypes.UUID,
             defaultValue: DataTypes.UUIDV4,
-            primaryKey: true,
+            allowNull: false,
         },
         nickname: {
             type: DataTypes.STRING,
