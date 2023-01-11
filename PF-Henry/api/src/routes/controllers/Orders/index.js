04fef2888382e1784b11f37cd841366a4d5390d7
Benjamin Miño <natanael.96.01@gmail.com>
2023-01-11 00:27:21 -0300

    updated route Order create

--- PF-Henry/api/src/routes/controllers/Orders/index.js
+++ PF-Henry/api/src/routes/controllers/Orders/index.js
@@ -48,9 +48,11 @@ const getAllOrders = async () => {
     return orders;
 };
 
-const postOrder = async (totalAmount, state, productId, userId, totalProducts) => {
+const postOrder = async (totalAmount, state="Pending", userId, totalProducts) => {
     const newOrder = await Order.create({ totalAmount, state, totalProducts });
-    await newOrder.setProducts(productId);
+    for(let i = 0; i < totalProducts.length; i++){
+        await newOrder.addProduct(totalProducts[i].id);
+    }
     await newOrder.setUser(userId);
     return newOrder;
 };
