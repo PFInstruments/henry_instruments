06a6031f480eb575e539060c175ea90164fba88b
Andyvaz1 <andyvaz192@gmail.com>
2023-01-12 04:34:36 -0300

    Paginado fixed, productos no renderizan si no tienen stock

--- PF-Henry/client/src/Components/Paginado/Paginado.jsx
+++ PF-Henry/client/src/Components/Paginado/Paginado.jsx
@@ -1,27 +1,23 @@
 import React from "react";
 
-export default function Paginado({
-  productsPerPage,
-  localProducts,
-  paginado,
-}) {
-  const pageNumbers = [];
+export default function Paginado({ productsPerPage, localProducts, paginado }) {
+    const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(localProducts / productsPerPage); i++) {
-    pageNumbers.push(i);
-  }
-  return (
-    <div>
-      <ul className="pagination" >
-        {pageNumbers &&
-          pageNumbers.map((p) => (
-            <li key={p}  onClick={() => paginado(p)}>
-              <span  href="#" className="page-link"  >
-                {p}
-              </span>
-            </li>
-          ))}
-          </ul>
-    </div>
-  );
+    for (let i = 1; i <= Math.ceil(localProducts / productsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+    return (
+        <div>
+            <ul className="pagination">
+                {pageNumbers &&
+                    pageNumbers.map((p) => (
+                        <li key={p} onClick={() => paginado(p)}>
+                            <span href="#" className="page-link tw-ml-4">
+                                {p}
+                            </span>
+                        </li>
+                    ))}
+            </ul>
+        </div>
+    );
 }
