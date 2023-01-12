374d6000dd0470af95ece1684cb21b5e891b09f7
Andyvaz1 <andyvaz192@gmail.com>
2023-01-12 07:11:22 -0300

    pagination fix

--- PF-Henry/client/src/Components/CardGrid/CardGrid.jsx
+++ PF-Henry/client/src/Components/CardGrid/CardGrid.jsx
@@ -12,24 +12,24 @@ export default function CardGrid({ localProducts }) {
     // const indexOfFirst = indexOfLast - productsPerPage;
     //  const currentProducts = localProducts.slice(indexOfFirst, indexOfLast);
 
-    function paginado(pageNumber) {
-        setCurrentPage(pageNumber);
-    }
-
     useEffect(() => {
         setCurrentPage(1);
     }, [localProducts]);
-
+    const max = Math.ceil(localProducts.length / productsPerPage);
     return (
         <div>
             <br />
 
             <div className="tw-flex tw-justify-center">
-                <Paginado
-                    productsPerPage={productsPerPage}
-                    paginado={paginado}
-                    localProducts={localProducts.length}
-                />
+                {max > 0 ? (
+                    <Paginado
+                        max={max}
+                        currentPage={currentPage}
+                        setCurrentPage={setCurrentPage}
+                    />
+                ) : (
+                    <span></span>
+                )}
             </div>
             <div className="tw-grid tw-grid-flow-row tw-gap-2 tw-text-neutral-600 sm:tw-grid-cols-1 md:tw-grid-cols-2 lg:tw-grid-cols-3 xl:tw-grid-cols-3">
                 {!localProducts ? (
@@ -37,7 +37,7 @@ export default function CardGrid({ localProducts }) {
                 ) : localProducts[0] === "Product Not Found" ? (
                     <div className="position-relative m-5">
                         <div className="position-absolute top-50 start-50 translate-middle">
-                            <h3>This instrument does not exist</h3>
+                            <h3>Product Not Found</h3>
                         </div>
                     </div>
                 ) : (
@@ -67,12 +67,15 @@ export default function CardGrid({ localProducts }) {
                 )}
             </div>
             <div className="tw-flex tw-justify-center">
-                <Paginado
-                    productsPerPage={productsPerPage}
-                    paginado={paginado}
-                    localProducts={localProducts.length}
-                    setProductsPerPage={setProductsPerPage}
-                />
+                {max > 0 ? (
+                    <Paginado
+                        max={max}
+                        currentPage={currentPage}
+                        setCurrentPage={setCurrentPage}
+                    />
+                ) : (
+                    <span></span>
+                )}
             </div>
         </div>
     );
